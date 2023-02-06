02d9df263176836b1478c952eaff7484885a854e
balpe22 <115620094+balpe22@users.noreply.github.com>
2023-02-06 10:11:10 +0100

    USD_RESOLVER_EXAMPLE => AYON_ASSET_RESOLVER renaming

--- src/AYON_AssetResolver.cpp
+++ src/AYON_AssetResolver.cpp
@@ -22,7 +22,7 @@ PXR_NAMESPACE_OPEN_SCOPE
 
 AR_DEFINE_RESOLVER(AYON_AssetResolver, ArResolver);
 
-TF_DEBUG_CODES(USD_RESOLVER_EXAMPLE);
+TF_DEBUG_CODES(AYON_ASSET_RESOLVER);
 
 TF_DEFINE_ENV_SETTING(USD_RESOLVER_EXAMPLE_ASSET_DIR, ".","Root of asset directory used by AYON_AssetResolver.")
 TF_DEFINE_PRIVATE_TOKENS(_tokens, (asset) (latest) ((version, "{$VERSION}")));
@@ -39,14 +39,14 @@ static std::string _CreateIdentifierHelper(const std::string& assetPath, const A
 
 std::string AYON_AssetResolver::_CreateIdentifier(const std::string& assetPath, const ArResolvedPath& anchorAssetPath) const
 {
-    TF_DEBUG(USD_RESOLVER_EXAMPLE).Msg("AYON_AssetResolver::_CreateIdentifier('%s', '%s')\n", assetPath.c_str(), anchorAssetPath.GetPathString().c_str());
+    TF_DEBUG(AYON_ASSET_RESOLVER).Msg("AYON_AssetResolver::_CreateIdentifier('%s', '%s')\n", assetPath.c_str(), anchorAssetPath.GetPathString().c_str());
 
     return _CreateIdentifierHelper(assetPath, anchorAssetPath);
 }
 
 std::string AYON_AssetResolver::_CreateIdentifierForNewAsset(const std::string& assetPath, const ArResolvedPath& anchorAssetPath) const
 {
-    TF_DEBUG(USD_RESOLVER_EXAMPLE).Msg("AYON_AssetResolver::_CreateIdentifierForNewAsset" "('%s', '%s')\n", assetPath.c_str(), anchorAssetPath.GetPathString().c_str());
+    TF_DEBUG(AYON_ASSET_RESOLVER).Msg("AYON_AssetResolver::_CreateIdentifierForNewAsset" "('%s', '%s')\n", assetPath.c_str(), anchorAssetPath.GetPathString().c_str());
 
     return _CreateIdentifierHelper(assetPath, anchorAssetPath);
 }
@@ -64,14 +64,14 @@ ArResolvedPath AYON_AssetResolver::_Resolve(const std::string& assetPath) const
 
 ArResolvedPath AYON_AssetResolver::_ResolveForNewAsset(const std::string& assetPath) const
 {
-    TF_DEBUG(USD_RESOLVER_EXAMPLE).Msg("AYON_AssetResolver::_ResolveForNewAsset('%s')\n", assetPath.c_str());
+    TF_DEBUG(AYON_ASSET_RESOLVER).Msg("AYON_AssetResolver::_ResolveForNewAsset('%s')\n", assetPath.c_str());
 
     return ArResolvedPath(assetPath);
 }
 
 std::shared_ptr<ArAsset> AYON_AssetResolver::_OpenAsset(const ArResolvedPath& resolvedPath) const
 {
-    TF_DEBUG(USD_RESOLVER_EXAMPLE).Msg("AYON_AssetResolver::OpenAsset('%s')\n", resolvedPath.GetPathString().c_str());
+    TF_DEBUG(AYON_ASSET_RESOLVER).Msg("AYON_AssetResolver::OpenAsset('%s')\n", resolvedPath.GetPathString().c_str());
 
     std::string filesystemPath = "";
     return ArFilesystemAsset::Open(ArResolvedPath(std::move(filesystemPath)));
@@ -79,10 +79,10 @@ std::shared_ptr<ArAsset> AYON_AssetResolver::_OpenAsset(const ArResolvedPath& re
 
 std::shared_ptr<ArWritableAsset> AYON_AssetResolver::_OpenAssetForWrite(const ArResolvedPath& resolvedPath, WriteMode writeMode) const
 {
-    TF_DEBUG(USD_RESOLVER_EXAMPLE).Msg("AYON_AssetResolver::_OpenAssetForWrite('%s', %d)\n", resolvedPath.GetPathString().c_str(), static_cast<int>(writeMode));
+    TF_DEBUG(AYON_ASSET_RESOLVER).Msg("AYON_AssetResolver::_OpenAssetForWrite('%s', %d)\n", resolvedPath.GetPathString().c_str(), static_cast<int>(writeMode));
 
     std::string filesystemPath = "";
 
-    TF_DEBUG(USD_RESOLVER_EXAMPLE).Msg("  - Opening file for write at %s\n", filesystemPath.c_str());
+    TF_DEBUG(AYON_ASSET_RESOLVER).Msg("  - Opening file for write at %s\n", filesystemPath.c_str());
     return ArFilesystemWritableAsset::Create(ArResolvedPath(std::move(filesystemPath)), writeMode);
 }
