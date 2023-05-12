3167e6160ca05962a6f4d7091eec10e7a874ff54
Oscar Domingo <oscardomingo@gmail.com>
2023-05-12 16:16:46 +0100

    Match current AYON API endpoint request/response This commit will ensure that we can configure the REST API via the environment variables "AYON_SERVER_URL" and "AYON_API_KEY".
    It will also do the POST request against the correct endpoint, and will
    correctly parse the response.
    
    A bit of cleanup has been don with the use of `namespace`.

--- src/AYON_AssetResolvingProvider_REST.cpp
+++ src/AYON_AssetResolvingProvider_REST.cpp
@@ -1,7 +1,11 @@
-﻿#include "AYON_AssetResolvingProvider_REST.h"
+#include "AYON_AssetResolvingProvider_REST.h"
 #include <boost/exception/diagnostic_information.hpp> 
 #include <iostream>
 #include "Logging/AYON_Logging.h"
+
+namespace Asio = boost::asio;
+namespace Beast = boost::beast;
+namespace Http = boost::beast::http;
 
 
 AYON_AssetResolveResult AYON_AssetResolvingProvider_REST::Resolve(const std::string& AssetPath) {
@@ -9,11 +13,36 @@ AYON_AssetResolveResult AYON_AssetResolvingProvider_REST::Resolve(const std::str
     AYON_AssetResolveResult Result;
     try 
     {
-        //TCP
-        //-------------------
-        boost::asio::io_context ioc;
-        boost::asio::ip::tcp::resolver resolver(ioc);
-        boost::beast::tcp_stream stream(ioc);
+        // Set up the TCP 
+        Asio::io_context ioc;
+        Asio::ip::tcp::resolver resolver(ioc);
+        Beast::tcp_stream stream(ioc);
+        Host = "localhost";
+        Port = 5000;
+
+        // Set up the API request based on the ENV VAR
+        char * ayon_server_env_var = getenv("AYON_SERVER_URL");
+        std::string ayon_server = ayon_server_env_var == NULL ? std::string("") : std::string(ayon_server_env_var);
+        
+        if (!ayon_server.empty()) {
+            size_t colon_char_index = ayon_server.find_first_of(":");
+
+            if (colon_char_index) {
+                // Split URL and Port
+                Host = ayon_server.substr(0, colon_char_index);
+                Port = std::stoi(ayon_server.substr(colon_char_index + 1));
+            } else {
+                // Use the provided URL with default PORT (5000)
+                Host = ayon_server;
+                //Port where is listening server
+                Port = 5000;
+            }
+        }
+        
+
+        std::cout << "Host: " + Host << std::endl;
+        std::cout << "Port: " + Port << std::endl;
+
         //convert DNS name to ip address and port
         auto const results = resolver.resolve(Host, std::to_string(Port));
         stream.connect(results);
@@ -21,22 +50,23 @@ AYON_AssetResolveResult AYON_AssetResolvingProvider_REST::Resolve(const std::str
         // HTTP Request
         //-------------------
         auto req = Request_Create(AssetPath);
-        boost::beast::http::write(stream, req);
+        Http::write(stream, req);
 
         //HTTP Response Reading
         //-------------------
-        boost::beast::flat_buffer buffer;
-        boost::beast::http::response<boost::beast::http::dynamic_body> res;
-        boost::beast::http::read(stream, buffer, res);
+        Beast::flat_buffer buffer;
+        Http::response<Http::dynamic_body> res;
+        Http::read(stream, buffer, res);
         AYON_LOG_INFO("Response", res);
 
         // Gracefully close the socket
-        boost::beast::error_code ec;
-        stream.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
+        Beast::error_code ec;
+        stream.socket().shutdown(Asio::ip::tcp::socket::shutdown_both, ec);
 
         //Parsing JSON
         //-------------------
-        Result.ResolvedPath = Response_Parse(res)+".7";
+        
+        Result.ResolvedPath = Response_Parse(res, AssetPath);
         Result.bResolved = true;
     }
     catch (const boost::exception& Ex) 
@@ -51,35 +81,54 @@ AYON_AssetResolveResult AYON_AssetResolvingProvider_REST::Resolve(const std::str
     return Result;
 }
 
-boost::beast::http::request<boost::beast::http::string_body> AYON_AssetResolvingProvider_REST::Request_Create(const std::string& AssetPath)
+Http::request<Http::string_body> AYON_AssetResolvingProvider_REST::Request_Create(const std::string& AssetPath)
 {
-    boost::beast::http::request<boost::beast::http::string_body> req{ boost::beast::http::verb::get, ResolvingUrlEndpointPath, Version };
-    req.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-    req.set("unresolved_path", AssetPath);
-    AYON_LOG_INFO("Request", req);
+    Http::request<Http::string_body> req {
+        Http::verb::post,
+        UsdApiEndpoint,
+        Version
+    };
+
+
+    req.set(Http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+    req.set(Http::field::content_type, "application/json");
+
+    char * ayon_api_key_env_var = getenv("AYON_API_KEY");
+    std::string ayon_api_key = ayon_api_key_env_var == NULL ? std::string("") : std::string(ayon_api_key_env_var);
+
+    if (!ayon_api_key.empty()) {
+        req.set("X-Api-Key", ayon_api_key);
+    }
+
+    req.body() = "{\"uris\": [\"" + AssetPath + "\"]}";
+    req.prepare_payload();
+    AYON_LOG_INFO("Sending request", req);
 
     return req;
 }
 
-std::string AYON_AssetResolvingProvider_REST::Response_Parse(const boost::beast::http::response<boost::beast::http::dynamic_body>& Response)
+std::string AYON_AssetResolvingProvider_REST::Response_Parse(const Http::response<Http::dynamic_body>& Response, const std::string& AssetPath)
 {
-    auto pt = GetPropertyTreeFromResponse(Response);
-    auto resolved_path = pt.get<std::string>("resolved_path");
-    AYON_LOG_INFO("Resolved Path", resolved_path);
+    std::stringstream response_ss;
+    response_ss << Beast::buffers_to_string(Response.body().data());
 
-    return resolved_path;
+    boost::property_tree::ptree response_ptree;
+    boost::property_tree::read_json(response_ss, response_ptree);
+
+    /* Explaination of how the JSON maps to a ptree, and how to traverse the
+    list in the "paths" key: https://stackoverflow.com/a/59227626 */
+    auto paths = response_ptree.get_child("paths");
+
+    for (auto path_uri = paths.begin(); path_uri != paths.end(); ++path_uri) {
+        for (auto path_resolve = path_uri->second.begin(); path_resolve != path_uri->second.end(); ++path_resolve) {
+            if (path_resolve->first == AssetPath) {
+                AYON_LOG_INFO("Resolved Path", path_resolve->second.data());
+                return path_resolve->second.data();
+            }
+        }
+    }
+
+    return "";
 }
 
-boost::property_tree::ptree AYON_AssetResolvingProvider_REST::GetPropertyTreeFromResponse(const boost::beast::http::response<boost::beast::http::dynamic_body>& Response)
-{
-    auto body_str = boost::beast::buffers_to_string(Response.body().data());
-    AYON_LOG_INFO("Response body", body_str);
-
-    std::stringstream ss;
-    ss << body_str;
-    boost::property_tree::ptree pt;
-    boost::property_tree::read_json(ss, pt);
-
-    return pt;
-}
 
