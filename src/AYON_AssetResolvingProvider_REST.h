86e74a4db368d717e0713e4fb952e064397a7568
Oscar Domingo <oscardomingo@gmail.com>
2023-06-08 16:33:24 +0100

    Remove `boost/json` import
    It's not being used and it prevents the plugin from building with
    Houdini's USD.

--- src/AYON_AssetResolvingProvider_REST.h
+++ src/AYON_AssetResolvingProvider_REST.h
@@ -6,7 +6,6 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
-#include <boost/json.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include "AYON_AssetResolvingProvider.h"
 
