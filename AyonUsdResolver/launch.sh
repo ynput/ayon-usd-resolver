d26a52f4f064896d97daccd8b3f2c9c5c4fdda3d
workh <workh@linux.fritz.box>
2024-01-26 18:23:40 +0100

    corrected project.kdl for zellij (display) // corrected the startup script

--- AyonUsdResolver/launch.sh
+++ AyonUsdResolver/launch.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-# Setup environment
-
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-
+#---------- Set Usd/Python Varibles for Resolver Load
 export USD_ASSET_RESOLVER=$SCRIPT_DIR
-export TF_DEBUG=CACHEDRESOLVER_RESOLVER_CONTEXT
-# export TF_DEBUG=CACHEDRESOLVER_RESOLVER
-export LD_LIBRARY_PATH=$SCRIPT_DIR/cachedResolver/lib:$LD_LIBRARY_PATH
-export PXR_PLUGINPATH_NAME=$SCRIPT_DIR/cachedResolver/resources:$PXR_PLUGINPATH_NAME
+export LD_LIBRARY_PATH=$SCRIPT_DIR/ayonUsdResolver/lib:$LD_LIBRARY_PATH
+export PXR_PLUGINPATH_NAME=$SCRIPT_DIR/ayonUsdResolver/resources:$PXR_PLUGINPATH_NAME
+export PYTHONPATH=$SCRIPT_DIR/ayonUsdResolver/lib/python:
 
+#---------- Set Debug Code
+export TF_DEBUG=AYONUSDRESOLVER_RESOLVER_CONTEXT
+# export TF_DEBUG=AYONUSDRESOLVER_RESOLVER
 
+#---------- Launch Houdini
+cd /opt/hfs20.0
+source houdini_setup
+houdini -foreground
 
-export PYTHONPATH=$SCRIPT_DIR/cachedResolver/lib/python:
-
-# Launch Houdini
-pushd /opt/hfs20.0.547 && source houdini_setup && popd
-#houdini -foreground -desktop solaris
-houdini -foreground /home/lyonh/Documents/dell/test.hiplc
-# houdini -foreground -desktop solaris /home/lyonh/Documents/resolver_test_usd_things/resolver_testing.hiplc
+#houdini -foreground /home/lyonh/Documents/dell/test.hiplc
+#houdini -foreground -desktop solaris /home/lyonh/Documents/resolver_test_usd_things/resolver_testing.hiplc
 
