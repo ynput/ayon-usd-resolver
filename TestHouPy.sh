0b5748ea8db133bcb50de757a0b98dedbaab5cfd
workh <workh@linux.fritz.box>
2024-02-23 15:20:16 +0100

    adding compile definitions to make building agasint hou19 195 posible

--- TestHouPy.sh
+++ TestHouPy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-SCRIPT_DIR=$SCRIPT_DIR/dist
+SCRIPT_DIR=$SCRIPT_DIR/Resolvers
 
 #---------- Set Usd/Python Variables for Resolver Load
 export USD_ASSET_RESOLVER=$SCRIPT_DIR
@@ -9,22 +9,25 @@ export LD_LIBRARY_PATH=$SCRIPT_DIR/ayonUsdResolver/lib:$LD_LIBRARY_PATH
 export PXR_PLUGINPATH_NAME=$SCRIPT_DIR/ayonUsdResolver/resources:$PXR_PLUGINPATH_NAME
 export PYTHONPATH=$SCRIPT_DIR/ayonUsdResolver/lib/python:
 
-export AYON_API_KEY="f2edece31ee8376292c4d545d711ca36cf6a7f47485f565bf356512b7963bec1"
+export AYON_API_KEY="fa12949bbf12ee79f013a8713e7a13e4c3edb42efa539f491334a81294179e36"
 export AYON_SERVER_URL="http://192.168.178.42:5000"
 
-export AYONLOGGERLOGLVL="OFF"
+export AYONLOGGERLOGLVL="INFO"
 export AYONLOGGERFILELOGGING="ON"
 export AYONLOGGERFILEPOS="LoggingFiles"
 
 # Source Houdini setup script
-pushd /opt/hfs20.0 && source houdini_setup && popd
+pushd /opt/hfs19.5.805 && source houdini_setup && popd
+
+#19.5.805
+#20.0.590
 
 # Start Houdini with Hython
 hython << EOF
 import random
 from pxr import Ar
 
-num_random_numbers = 5
+num_random_numbers = 1
 
 Ar.SetPreferredResolver("AyonUsdResolver")
 resolver = Ar.GetResolver()
@@ -35,7 +38,8 @@ resolver = Ar.GetResolver()
 
 # Generate and print random numbers
 for i in range(num_random_numbers):
-	rpath = f"ayon://Usd_Base/trees?product=usdtree_{i}&version=*&representation=usd"
+	rpath = f"ayon://Usd_Base/trees?product=usdtree_1&version=*&representation=usd"
+	#rpath = f"ayon://Usd_Base/trees?product=usdtree_{i}&version=*&representation=usd"
 	print("\033[95m" + "Requested Path:" + "\033[0m")
 	print(rpath)
 	
--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 BUILD_DIR=$SCRIPT_DIR/build
 
-
 #----------- Check for os Type ------------------------
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     OS="Linux"
@@ -31,11 +30,6 @@ JTRACE=0
 SOFTWARE_TARGET="" 
 TEST=0
 
-
-#------------------ Development Env Wars ----------------- 
-
-
-
 #----------------- Build Script Statup Tests ------------------
 if [ "$1" == "Debug" ]; then
   echo "Build running in debug mode"
@@ -51,15 +45,16 @@ if [[ " $@ " =~ " Debug " && " $@ " =~ " Clean " ]]; then
   CLEAN_BUILD=1
 fi
 
-
 #------------------- Houdini Config ------------------------
-HOU_START=0
-HOU_VER=20.0 # Set the houdini version // needs to be the same as the hfs foulder version in your install
+HOU_VER=19.5.805 # Set the houdini version // needs to be the same as the hfs foulder version in your install
+
+#19.5.805
+#20.0.590
+
 
 if [ "$OS" == "Linux" ]; then 
   export HFS="/opt/hfs${HOU_VER}"
 fi
-
 
 
 #-------------- Debug Over Writes Main
@@ -67,23 +62,21 @@ if [ "$DEBUG" -eq 1 ]; then
   DEV=1
   source devOpVars.sh
   echo " dev var is set to $DEV"
-
-  #TODO function that looks if houdini is the SOFTWARE_TARGET
-  HOU_START=1
 fi
 
 #------------- Set DevOps Varibles 
 
 export DEV=$DEV
 
-
 #--------------- Build Execution ------------------------ 
 if [ "$CLEAN_BUILD" -eq 1 ]; then
   echo "Clean build is activated"
   rm -rf bin
   rm -rf build
+  rm -rf Resolvers
   mkdir bin
   mkdir build
+  mkdir Resolvers
 fi
 
 
@@ -92,48 +85,9 @@ cmake . -B build -DDEV=$DEV -DJTRACE=$JTRACE
 
 
 if [ "$CLEAN_BUILD" -eq 1 ]; then 
-  cmake --build build --clean-first --parallel
+  cmake --build build --clean-first
 else
-  cmake --build build --parallel
+  cmake --build build
 fi
 
 cmake --install build   
-#----------- Copy / Iinstall Setup------------------------------- 
-#TODO move this to use Cmake -Install 
-echo ""
-RESOLVER_EXEC_PATH="$SCRIPT_DIR/AyonUsdResolver/ayonUsdResolver/lib/ayonUsdResolver.so"
-RESOLVER_PY_PATH="$SCRIPT_DIR/AyonUsdResolver/ayonUsdResolver/lib/python/_ayonUsdResolver.so"
-
-# Remove Exec build
-#if [ -e "$RESOLVER_EXEC_PATH" ]; then
-#    rm "$RESOLVER_EXEC_PATH"
-#    echo "Removed File $RESOLVER_EXEC_PATH."
-#else
-#    echo "File $RESOLVER_EXEC_PATH does not exist."
-#fi
-# Remove Py build
-#if [ -e "$RESOLVER_PY_PATH" ]; then
-#    rm "$RESOLVER_PY_PATH"
-#    echo "Removed File $RESOLVER_PY_PATH."
-#else
-#    echo "File $RESOLVER_PY_PATH does not exist."
-#fi
-#Copy Newlie build files into there respective paths 
-#cp $BUILD_DIR/src/AyonUsdResolver/ayonUsdResolver.so $RESOLVER_EXEC_PATH
-#echo "copyied build .so file to $RESOLVER_EXEC_PATH"
-#cp $SCRIPT_DIR/build/src/AyonUsdResolver/_ayonUsdResolver.so $RESOLVER_PY_PATH
-#echo "copyied build .so file to $RESOLVER_PY_PATH"
-
-
-#-------------- Running Tests --------------- 
-#TODO implement Tests 
-
-if [ "$TEST" -eq 1 ]; then 
-  ctest -VV --test-dir build
-fi
-
-# if [ "$HOU_START" -eq 1 ]; then
-#   echo "statring houdini"
-#   cd $SCRIPT_DIR/AyonUsdResolver
-#   ./launch.sh
-# fi
