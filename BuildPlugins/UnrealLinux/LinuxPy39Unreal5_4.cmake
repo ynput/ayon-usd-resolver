4a3dffcb169136d7ed33a03f32f12be4c71e206a
Lyon_Rosenblatt <48656065+lyon040502003@users.noreply.github.com>
2024-05-02 08:01:15 +0200

    moved complie plugin include in order to allow for setting compiler in BuildPlugin

---
+++ BuildPlugins/UnrealLinux/LinuxPy39Unreal5_4.cmake
@@ -0,0 +1,44 @@
+# LinuxPy310Houdini20
+
+# Variables that need to be set by the plugin (the env variables will be checked by the main cmakeLists script and an error will accrue if one ore more are missing)
+# AR_PXR_INCLUDE_DIR
+# AR_PXR_LIB_DIR
+# AR_PYTHON_LIB_NUMBER
+# AR_BOOST_INCLUDE_DIR
+# BOOST_LIB_DIR
+# AR_PYTHON_LIB_DIR
+# AR_PYTHON_INCLUDE_DIR
+# AR_PXR_LIB_PREFIX
+
+
+# UNREALENGINEENGINEPATH /home/lyonh/Documents/UnrealEngine/Engine 
+
+set(CMAKE_CXX_COMPILER "clang++")
+# plugin dependent settings 
+set(AR_UNREAL_ENGINE_ENGINE_FOULDER $ENV{UNREALENGINEENGINEPATH} CACHE PATH "Maya install directory")
+set(AR_UNREAL_ENGINE_USDIMPORTER_THIRDPARTY_FOULDER ${AR_UNREAL_ENGINE_ENGINE_FOULDER}/Plugins/Importers/USDImporter/Source/ThirdParty )
+if (NOT DEFINED ENV{UNREALENGINEENGINEPATH})
+  message(FATAL_ERROR "MAYAPATH Env Variable is not defined. But BuildPlugin LinuxPy310Houdini20 needs it to function.") 
+endif()
+
+
+set(AR_PXR_LIB_DIR ${AR_UNREAL_ENGINE_USDIMPORTER_THIRDPARTY_FOULDER}/Linux/bin/x86_64-unknown-linux-gnu)
+set(AR_PXR_LIB_PREFIX "usd_") #the library prefix is divergent between Linux and win, some times software developers also rename the prefix
+set(AR_PXR_INCLUDE_DIR ${AR_UNREAL_ENGINE_USDIMPORTER_THIRDPARTY_FOULDER}/USD/include)
+
+# TBB include dir
+include_directories(${AR_UNREAL_ENGINE_ENGINE_FOULDER}/Source/ThirdParty/Intel/TBB/IntelTBB-2019u8/include)
+
+# setting up python 
+set(AR_PYTHON_LIB_NUMBER python39)
+set(AR_PYTHON_LIB_DIR ${AR_UNREAL_ENGINE_ENGINE_FOULDER}/Source/ThirdParty/Python3/Linux/include/python3.9)
+set(AR_PYTHON_INCLUDE_DIR ${AR_UNREAL_ENGINE_ENGINE_FOULDER}/Source/ThirdParty/Python3/Linux/include/python3.9)
+#set(AR_PYTHON_LIB_SITEPACKAGES ${AR_PYTHON_LIB_DIR}/${AR_PYTHON_LIB}/site-packages)
+
+
+set(AR_BOOST_EXTEND -mt-x64) 
+set(AR_BOOST_INCLUDE_DIR ${AR_UNREAL_ENGINE_ENGINE_FOULDER}/Source/ThirdParty/Boost/boost-1_80_0/include)
+set(BOOST_LIB_DIR ${AR_UNREAL_ENGINE_ENGINE_FOULDER}/Binaries/Linux)
+
+add_compile_definitions(_GLIBCXX_USE_CXX11_ABI=1)
+target_compile_definitions(AyonCppApi PUBLIC _GLIBCXX_USE_CXX11_ABI=1)
