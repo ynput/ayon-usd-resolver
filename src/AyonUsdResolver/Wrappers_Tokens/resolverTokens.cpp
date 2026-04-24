3cfee28c8182f682de1dbe67ceba9e5df979c614
Tadeas Hejnic <tadeas.hejnic@gmail.com>
2026-04-24 16:49:00 +0200

    adjust comment

--- src/AyonUsdResolver/Wrappers_Tokens/resolverTokens.cpp
+++ src/AyonUsdResolver/Wrappers_Tokens/resolverTokens.cpp
@@ -9,9 +9,7 @@ mappingPairs("mappingPairs", TfToken::Immortal)
 
 const AyonUsdResolverTokensType&
 GetAyonUsdResolverTokens() {
-    // Houdini tears down plugin DLL state late in process shutdown.
-    // Keep the token table alive for the lifetime of the process and
-    // avoid TfStaticData destructor ordering during exit.
+    // Keep the token table alive for the lifetime of the process.
     static const auto* tokens = new AyonUsdResolverTokensType();
     return *tokens;
 }
