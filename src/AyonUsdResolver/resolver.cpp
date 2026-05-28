ae604cf6f4deafceb2ea604b8b27c6d2b6c7bc59
Tadeas Hejnic <tadeas.hejnic@gmail.com>
2026-05-28 13:28:21 +0200

    change the condition

--- src/AyonUsdResolver/resolver.cpp
+++ src/AyonUsdResolver/resolver.cpp
@@ -181,7 +181,7 @@ AyonUsdResolver::_Resolve(const std::string &assetPath) const {
         TF_DEBUG(AYONUSDRESOLVER_RESOLVER)
                 .Msg("Resolver::_Resolve( '%s' ) is relative path\n", pathToResolve->c_str());
         ArResolvedPath resolvedPath = _ResolveAnchored(ArchGetCwd(), *pathToResolve);
-        if (resolvedPath) {
+        if (TfPathExists(resolvedPath.GetPathString())) {
             return resolvedPath;
         }
 
